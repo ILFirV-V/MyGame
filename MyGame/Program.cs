93be8670c68c6549534fed40d6d9f7a4178cc544
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2022-04-25 23:01:29 +0500

    Revert "тестирую git"
    This reverts commit 2f16a531b90b3b7be9cc18cf73e1765bd0821793.

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -17,7 +17,6 @@ namespace MyGame
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // тестирую git
         }
     }
 }
